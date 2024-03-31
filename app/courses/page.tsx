ce57105d080b2be3909fd752ccd76b1ffe56aeca
Yash Ghugardare <yghugardare6@gmail.com>
2024-03-31 08:37:52 +0530

    reverse

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -100,7 +100,7 @@ const Page = (props: Props) => {
             <br />
             <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] 1500px:grid-cols-4 1500px:gap-[35px] mb-12 border-0">
               {courses &&
-                courses.map((item: any, index: number) => (
+                courses.reverse().map((item: any, index: number) => (
                   <CourseCard item={item} key={index} />
                 ))}
             </div>
